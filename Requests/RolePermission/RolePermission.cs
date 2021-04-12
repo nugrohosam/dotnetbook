23cdc3b37f26a3f20b4aab3172afff2839fbf9e2
nugroho <nugroho@mackbookpro.local>
2021-04-12 20:13:54 +0700

    Delete | Wheater forecast

--- Requests/RolePermission/RolePermission.cs
+++ Requests/RolePermission/RolePermission.cs
@@ -1,24 +1,24 @@
 using System.ComponentModel.DataAnnotations;
-using ValidationUser = BookApi.Validations.User;
+using ValidationPermission = BookApi.Validations.Permission;
 using ValidationRole = BookApi.Validations.Role;
 namespace BookApi.Requests.RolePermission
 {
     public class RolePermissionCreate
     {
         [Required]
-        [ValidationRole.IsExists("User not found")]
+        [ValidationPermission.IsExists("User not found")]
         public long Permissionid { get; set; }
         [Required]
-        [ValidationUser.IsExists("User not found")]
+        [ValidationRole.IsExists("User not found")]
         public long Roleid { get; set; }
     }
     public class RolePermissionUpdate
     {
         [Required]
-        [ValidationRole.IsExists("User not found")]
+        [ValidationPermission.IsExists("User not found")]
         public long Permissionid { get; set; }
         [Required]
-        [ValidationUser.IsExists("User not found")]
+        [ValidationRole.IsExists("User not found")]
         public long Roleid { get; set; }
     }
 }
